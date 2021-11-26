66ee2b843ce0dfae5e221850461155152a53d0a5
anirudhb <anirudhb@users.noreply.github.com>
2021-11-25 16:36:03 -0800

    caddy_types_codegen: incinerate comments

--- caddy_types_codegen/src/lib.rs
+++ caddy_types_codegen/src/lib.rs
@@ -3,28 +3,9 @@ use std::collections::{HashMap, HashSet};
 
 use proc_macro2::TokenStream;
 use quote::quote;
-//use reqwest::blocking as reqwest;
 use serde::Deserialize;
 
-//type ReqwestResult<T> = Result<T, ::reqwest::Error>;
-
 // MARK: Codegen
-
-// #[derive(Debug, Deserialize)]
-// struct ConfigStructure {
-// 	// status_code: usize,
-// 	// result: ConfigStructure2,
-// }
-
-// type Namespaces = HashMap<String, Vec<NamespaceStructure>>;
-
-// #[derive(Debug, Deserialize)]
-// struct ConfigStructure2 {
-// 	structure: ConfigTypeValue,
-// 	namespaces: Namespaces,
-// 	breadcrumb: HashMap<String, ConfigTypeValue>,
-// 	repo: String,
-// }
 
 #[derive(Debug, Deserialize, Clone)]
 struct ConfigTypeValue {
@@ -38,8 +19,8 @@ struct ConfigTypeValue {
 #[serde(rename_all = "snake_case")]
 enum ConfigTypeValueInner {
 	Struct {
-		// This is always present except for the top-level struct,
-		// so add the default name for the top-level
+		// This is always present and the default type name here is
+		// basically used as a code smell for invalid JSON
 		#[serde(default = "default_type_name")]
 		type_name: String,
 		#[serde(default = "Vec::new")]
@@ -59,10 +40,8 @@ enum ConfigTypeValueInner {
 	Module {
 		/* NB: Type name for modules is derived from the module namespace, since it's an enum */
 		module_namespace: String,
-		// module_inline_key: String,
 	},
 	ModuleMap {
-		// type_name: String,
 		module_namespace: String,
 	},
 }
@@ -70,14 +49,6 @@ enum ConfigTypeValueInner {
 fn default_type_name() -> String {
 	"CaddyConfig".to_owned()
 }
-
-// #[derive(Debug, Deserialize)]
-// struct NamespaceStructure {
-// 	name: String,
-// 	docs: Option<String>,
-// 	package: String,
-// 	repo: String,
-// }
 
 #[derive(Debug, Deserialize, Clone)]
 struct KeyValuePair {
@@ -100,21 +71,6 @@ fn clean_up_go_type_name_path(path: &str) -> Cow<str> {
 		tmp_path.into()
 	}
 }
-
-/// Make a prefixed type (crate-local)
-// fn mk_prefixed_ty(ty: TokenStream, prefix: Option<&TokenStream>, add_crate: bool) -> TokenStream {
-// 	let tmp = if let Some(prefix) = prefix {
-// 		quote! { #prefix::#ty }
-// 	} else {
-// 		// quote! { crate::#ty }
-// 		ty
-// 	};
-// 	if add_crate {
-// 		quote! { crate::#tmp }
-// 	} else {
-// 		tmp
-// 	}
-// }
 
 /// Hack: fix up empty module namespace into "apps"
 fn fixup_module_namespace(module_namespace: &str) -> &str {
@@ -169,17 +125,8 @@ fn mk_type_name(config: &ConfigTypeValueInner) -> Cow<str> {
 	}
 }
 
-/// Generate a module name from a type name
-// fn mk_module_name(ty_name: &str) -> String {
-// 	use heck::SnakeCase;
-// 	ty_name.to_snake_case()
-// }
-
 /// Generate a type from the given config
-fn mk_type(
-	config: &ConfigTypeValueInner,
-	boxed: bool, /* , prefix: Option<&TokenStream> */
-) -> TokenStream {
+fn mk_type(config: &ConfigTypeValueInner, boxed: bool) -> TokenStream {
 	use ConfigTypeValueInner::*;
 	match config {
 		/* big int? */
@@ -187,13 +134,6 @@ fn mk_type(
 		Struct { .. } | Module { .. } | ModuleMap { .. } => {
 			let ty_name = mk_type_name(config);
 			let ty_name_lit = quote::format_ident!("{}", ty_name);
-			// let is_mod = matches!(config, Module { .. } | ModuleMap { .. });
-			// mk_prefixed_ty(
-			// 	quote! { #ty_name_lit },
-			// 	/* Modules / module maps are always top level */
-			// 	if is_mod { None } else { prefix },
-			// 	is_mod,
-			// )
 			let q = quote! { crate::#ty_name_lit };
 			if boxed {
 				quote! { crate::Identified<::std::boxed::Box<#q>> }
@@ -206,12 +146,12 @@ fn mk_type(
 		Int => quote! { i32 },
 		Float => quote! { f32 },
 		Array { elems } => {
-			let elems_ty = mk_type(&elems.value, false /* , prefix */);
+			let elems_ty = mk_type(&elems.value, false);
 			quote! { ::std::vec::Vec<#elems_ty> }
 		}
 		Map { map_keys, elems } => {
-			let key_ty = mk_type(&map_keys.value, false /* , prefix */);
-			let val_ty = mk_type(&elems.value, false /* , prefix */);
+			let key_ty = mk_type(&map_keys.value, false);
+			let val_ty = mk_type(&elems.value, false);
 			quote! { ::std::collections::HashMap<#key_ty, #val_ty> }
 		}
 	}
@@ -219,11 +159,7 @@ fn mk_type(
 
 /// Generate the inner struct-like block for a struct
 /// Invariant assumed that the passed config is a struct config
-fn mk_struct_block(
-	config: &ConfigTypeValue,
-	// mod_name: Option<&str>,
-	ty_names_set: &mut HashSet<String>,
-) -> Generated {
+fn mk_struct_block(config: &ConfigTypeValue, ty_names_set: &mut HashSet<String>) -> Generated {
 	use ConfigTypeValueInner::*;
 	if let Struct { struct_fields, .. } = &config.value {
 		let mut aux = vec![];
@@ -231,51 +167,21 @@ fn mk_struct_block(
 		let mut field_attrs = vec![];
 		let mut field_tys = vec![];
 		let mut field_docs = vec![];
-		// let mod_name_lit = mod_name
-		// 	.map(|n| quote::format_ident!("{}", n))
-		// 	.map(|i| quote! { #i });
 		for field in struct_fields {
-			// let should_insert_defs = if matches!(&field.value.value, Struct { .. }) {
-			// 	let ty_name = mk_type_name(&field.value.value);
-			// 	let has_ty = ty_names_set.insert(ty_name.to_string());
-			// 	!has_ty
-			// } else {
-			// 	true
-			// };
 			let (field_def, field_aux) = mk_def(&field.value, ty_names_set);
 			aux.push(field_aux);
 			aux.push(field_def);
-			// if should_insert_defs {
-			// 	aux.push(field_aux);
-			// 	aux.push(field_def);
-			// }
-			// let crate_quote = quote! { crate };
 			let is_boxed = if matches!(
 				&field.value.value,
 				Struct { .. } | Module { .. } | ModuleMap { .. }
 			) {
 				let ty_name1 = mk_type_name(&config.value);
 				let ty_name2 = mk_type_name(&field.value.value);
-				// eprintln!("{} {}", ty_name1, ty_name2);
 				ty_name1 == "SingleFieldEncoder" && ty_name2 == "CaddyLoggingEncoders"
-			// if ty_name1 == "SingleFieldEncoder" && ty_name2 == "CaddyLoggingEncoders" {
-			// 	// eprintln!("Boxing");
-			// 	true
-			// } else {
-			// 	false
-			// }
 			} else {
 				false
 			};
-			let ty = mk_type(
-				&field.value.value,
-				is_boxed, /* Modules and module maps are always defined at crate level */
-						  /* if matches!(&field.value.value, Module { .. } | ModuleMap { .. }) {
-							  None
-						  } else {
-							  mod_name_lit.as_ref()
-						  }, */
-			);
+			let ty = mk_type(&field.value.value, is_boxed);
 			// Fixes reserved keywords as field names
 			use heck::SnakeCase;
 			let snake_key = field.key.to_snake_case();
@@ -284,18 +190,6 @@ fn mk_struct_block(
 			field_names.push(quote! { #name_lit });
 			field_attrs.push(quote! { #[serde(rename = #field_key)] });
 			field_tys.push(ty);
-			// if matches!(&field.value.value, Struct { .. }) {
-			// 	let ty_name1 = mk_type_name(&config.value);
-			// 	let ty_name2 = mk_type_name(&field.value.value);
-			// 	if ty_name1 == "SingleFieldEncoder" && ty_name2 == "CaddyLoggingEncoders" {
-			// 		/* Hack: Prevent infinitely sized type */
-			// 		field_tys.push(quote! { ::std::boxed::Box<#ty> });
-			// 	} else {
-			// 		field_tys.push(ty);
-			// 	}
-			// } else {
-			// 	field_tys.push(ty);
-			// }
 			let field_doc = field.value.doc.as_deref().unwrap_or("");
 			field_docs.push(if field_doc.is_empty() {
 				quote! {}
@@ -305,21 +199,6 @@ fn mk_struct_block(
 		}
 		/* Prune empty modules */
 		let aux_collected = aux.into_iter().fold(quote! {}, |acc, x| quote! { #acc #x });
-		// let aux = if aux_collected.is_empty() {
-		// 	quote! {}
-		// } else {
-		// 	aux_collected
-		// 	// /* Put aux in a module if a name was provided */
-		// 	// if let Some(mod_name_lit) = mod_name_lit {
-		// 	// 	quote! {
-		// 	// 		pub mod #mod_name_lit {
-		// 	// 			#aux_collected
-		// 	// 		}
-		// 	// 	}
-		// 	// } else {
-		// 	// 	aux_collected
-		// 	// }
-		// };
 		(
 			quote! {
 				{
@@ -343,10 +222,8 @@ fn mk_struct_block(
 fn mk_struct_def(
 	config: &ConfigTypeValue,
 	rename_ty: Option<&str>,
-	// mod_name: Option<&str>,
 	ty_names_set: &mut HashSet<String>,
 ) -> Generated {
-	// let original_ty_name = mk_type_name(&config.value);
 	let ty_name = rename_ty
 		.map(|x| x.into())
 		.unwrap_or_else(|| mk_type_name(&config.value));
@@ -359,12 +236,7 @@ fn mk_struct_def(
 	} else {
 		quote! { #[doc = #doc] }
 	};
-	let (block, aux) = mk_struct_block(
-		config,
-		// mod_name.or_else(|| Some(&mk_module_name(&ty_name))), // mod_name.or(Some(&mk_module_name(&ty_name))),
-		ty_names_set,
-	);
-	//let has_ty = false; //ty_names_set.insert(ty_name.to_string());
+	let (block, aux) = mk_struct_block(config, ty_names_set);
 	(
 		quote! {
 			#doc
@@ -441,7 +313,6 @@ fn mk_module_aux(
 	ty_names_set: &mut HashSet<String>,
 ) -> Generated {
 	let name_fixed = mk_module_map_ty_name(name);
-	// let name_temp = mk_module_ty_name(name);
 	let config = ConfigTypeValue {
 		/* Modules don't have docs (for now), FIXME */
 		doc: None,
@@ -457,8 +328,6 @@ fn mk_module_aux(
 				.collect(),
 		},
 	};
-	/* rename the module using the passed name */
-	// let mod_name = mk_module_name(&mk_module_ty_name(name));
 	mk_struct_def(&config, None, ty_names_set)
 }
 
@@ -472,9 +341,6 @@ fn mk_module_def(
 	let ty_name = mk_module_ty_name(name);
 	let ty_name_lit = quote::format_ident!("{}", ty_name);
 	let ty_name_lit = quote! { #ty_name_lit };
-	// let mod_name = mk_module_name(&ty_name);
-	// let mod_name_lit = quote::format_ident!("{}", mod_name);
-	// let mod_name_lit = quote! { #mod_name_lit };
 	/* We don't need the struct, just the aux definitions */
 	let (_, aux) = mk_module_aux(name, values, ty_names_set);
 	let mut variants = Vec::new();
@@ -483,37 +349,11 @@ fn mk_module_def(
 		let camel_case_field_name_lit = quote::format_ident!("{}", camel_case_field_name);
 		let camel_case_field_name_lit = quote! { #camel_case_field_name_lit };
 		/* Use type as inner value */
-		let field_ty = mk_type(&field_config.value, false /* , Some(&mod_name_lit) */);
+		let field_ty = mk_type(&field_config.value, false);
 		variants.push(quote! {
 			#camel_case_field_name_lit(#field_ty)
 		});
 	}
-	// let mod_name = mk_module_name(&ty_name);
-	// let mod_name_lit = quote::format_ident!("{}", mod_name);
-	// let mod_name_lit = quote! { #mod_name_lit };
-	// let mut aux = Vec::new();
-	// let mut variants = Vec::new();
-	// for (field_name, field_config) in &values.values {
-	// 	let camel_case_field_name = field_name.to_camel_case();
-	// 	let camel_case_field_name_lit = quote::format_ident!("{}", camel_case_field_name);
-	// 	let camel_case_field_name_lit = quote! { #camel_case_field_name_lit };
-	// 	let (block, field_aux) = mk_struct_block(field_config, Some(&mod_name));
-	// 	aux.push(field_aux);
-	// 	variants.push(quote! {
-	// 		#camel_case_field_name_lit #block
-	// 	});
-	// }
-	// /* Prune empty modules */
-	// // let aux_collected = aux.into_iter().fold(quote! {}, |acc, x| quote! { #acc #x });
-	// // let aux = if aux_collected.is_empty() {
-	// // 	quote! {}
-	// // } else {
-	// // 	quote! {
-	// // 		pub mod #mod_name_lit {
-	// // 			#aux_collected
-	// // 		}
-	// // 	}
-	// // };
 	let tag = values
 		.inline_tag
 		.as_ref()
@@ -572,18 +412,6 @@ fn mk_toplevel(
 	}
 }
 
-// fn get_structure(url: &str) -> ReqwestResult<ConfigStructure> {
-// 	let c = reqwest::Client::new();
-// 	let body = c
-// 		.get(url)
-// 		.header(::reqwest::header::ACCEPT, "application/json")
-// 		.send()?;
-// 	Ok(body.json()?)
-// 	//.bytes()?;
-// 	//println!("bytes = {:?}", body);
-// 	//Ok(serde_path_to_error::deserialize(&mut serde_json::Deserializer::from_reader(body)).unwrap())
-// }
-
 // MARK: proc macro
 
 use std::env;
@@ -597,17 +425,6 @@ use syn::{Ident, LitStr, Token};
 trait ReadConfigTypeValue {
 	fn read_config(&self) -> Result<ConfigTypeValue, SynError>;
 }
-
-// impl<S: AsRef<std::path::Path> + std::fmt::Display> ReadConfigTypeValue for S {
-// 	fn read_config(&self) -> Result<ConfigTypeValue, ()> {
-// 		let manifest_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
-// 		let mut f = File::open(manifest_dir.join(self))
-// 			.map_err(|_| eprintln!("Failed to open {}", self))?;
-// 		let cfg = serde_json::from_reader(f)
-// 			.map_err(|_| eprintln!("Failed to deserialize {} as ConfigTypeValue", self))?;
-// 		Ok(cfg)
-// 	}
-// }
 
 impl ReadConfigTypeValue for LitStr {
 	fn read_config(&self) -> Result<ConfigTypeValue, SynError> {
@@ -647,13 +464,10 @@ struct NamespaceDef2 {
 impl Parse for NamespaceDef2 {
 	fn parse(input: ParseStream) -> SynResult<Self> {
 		let inline_tag = if input.peek(Ident) {
-			// let content;
-			// syn::parenthesized!(content in input);
 			let tag_ident = input.parse::<Ident>()?;
 			Some(tag_ident.to_string())
 		} else {
 			None
-			// Some("".to_owned()) // Hack: make mk_module_def happy
 		};
 		let content2;
 		let _parenthesized = syn::parenthesized!(content2 in input);
@@ -671,12 +485,6 @@ impl Parse for NamespaceDef2 {
 				))
 			}
 		};
-		// let inline_tag = if input.peek(syn::Ident) {
-		// 	let tag_ident = input.parse::<syn::Ident>()?;
-		// 	Some(tag_ident.to_string())
-		// } else {
-		// 	None
-		// };
 		let content;
 		let _braced = syn::braced!(content in input);
 		let values: Punctuated<NamespaceFieldDef2, Token![,]> =
@@ -755,14 +563,11 @@ impl CaddyTypesInput {
 
 #[proc_macro]
 pub fn caddy_types(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-	// let input2 = input.clone();
 	let input_parsed = syn::parse_macro_input!(input as CaddyTypesInput);
 	let (cfg, namespaces) = match input_parsed.into_structure() {
 		Ok(s) => s,
-		/* Errors have already been printed / spanned */
 		Err(e) => return e.into_compile_error().into(),
 	};
 	let out = mk_toplevel(&cfg, &namespaces);
-	// println!("{}", out);
 	out.into()
 }
